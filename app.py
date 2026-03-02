886e118fb1561d8b26536baac0c487202a987f17
KhushbooMehto <khushboo200022@gmail.com>
2026-03-02 22:08:03 +0530

    finalizing todo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template,request
+from flask import Flask ,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -38,6 +38,29 @@ def show():
     alltodo=Todo.query.all()
     print(alltodo)
     return "************* This page is use to show all data in TERMINAL **********"
+
+# -------- CRUD Operations -------
+@app.route("/update/<int:sno>",methods=["GET","POST"])
+def update(sno):
+    if request.method=="POST":
+        titel=request.form["title"]
+        desc=request.form["desc"]
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.titel=titel
+        todo.desc=desc
+        
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo=Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html",todo=todo)
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=True)
